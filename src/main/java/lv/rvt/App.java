e3ffc9b1fd46b846b5492ea14b851568bef67dbf
23DP2MMitr <166109771+23DP2MMitr@users.noreply.github.com>
2024-09-05 10:22:11 +0000

    Various Variables

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,29 +6,40 @@ public class App
 {
     public static void main( String[] args )
     {
-    
-        System.out.println("Hello RVT!");
-        Scanner scanner = new Scanner(System.in);
-    // System.out.println("Ievadi savu vārdu: ");
+        // System.out.println("Hello RVT!");
+        // Scanner scanner = new Scanner(System.in);
 
-       // String name = scanner.nextLine();
+       System.out.println("Chicken:");
+       int Chicken = 3;
+       System.out.println(Chicken);
 
-       // int age = 58;
-       // boolean isOnline = false;
-       // System.out.println("Sveiks " + name);
+       System.out.println("Bacon (kg):");
+       double Bacon = 5.5;
+       System.out.println(Bacon);
 
-        // Mēs gribam uzzināt no lietāja: Vards, Uzvards, Gruppu
-        // un beigās printēt visu kopā vienā rindiņā
+       System.out.println("Tractor:");
+       String Tractor = "None!";
+       System.out.println(Tractor);
 
-       System.out.println("Ievadi savu vardu:");
-       String name = scanner.nextLine();
-       System.out.println("Ievadi savu uzvardu:");
-       String surname = scanner.nextLine();
-       System.out.println("Ievadi savu gruppu:");
-       String grupa = scanner.nextLine();
+       System.out.println();
 
-       System.out.println(name + " " + surname + " " + grupa);
-
+      System.out.println("And finally, a summary:");
+      System.out.println(Chicken);
+      System.out.println(Bacon);
+      System.out.println(Tractor);
     }
 
 }
+
+
+// Uzdevums 0
+        // Mēs gribam uzzināt no lietāja: Vards, Uzvards, Gruppu
+        // un beigās printēt visu kopā vienā rindiņā
+       // System.out.println("Ievadi savu vardu:");
+       // String name = scanner.nextLine();
+       // System.out.println("Ievadi savu uzvardu:");
+       // String surname = scanner.nextLine();
+       // System.out.println("Ievadi savu gruppu:");
+       // String grupa = scanner.nextLine();
+
+      // System.out.println(name + " " + surname + " " + grupa);
