0009d3d59b361b552d4c7033ce26c09a79a71eac
23DP2MMitr <166109771+23DP2MMitr@users.noreply.github.com>
2024-09-05 09:42:35 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,12 +1,26 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+    
         System.out.println("Hello RVT!");
+        Scanner scanner = new Scanner(System.in);
+    // System.out.println("Ievadi savu vārdu: ");
 
+       // String name = scanner.nextLine();
+
+       // int age = 58;
+       // boolean isOnline = false;
+       // System.out.println("Sveiks " + name);
+
+        // Mēs gribam uzzināt no lietāja: Vards, Uzvards, Gruppu
+        // un beigās printēt visu kopā vienā rindiņa 
+
+       
     }
 
 }
