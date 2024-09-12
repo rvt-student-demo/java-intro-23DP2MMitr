0442227aa7821d490627b9d900c76e399f1bf122
23DP2MMitr <166109771+23DP2MMitr@users.noreply.github.com>
2024-09-12 08:30:43 +0000

    Addition formula

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,6 +7,44 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give the first number: ");
+        int nr1 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number: ");
+        int nr2 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println(nr1 + " + " + nr2 + " = " + (nr1 + nr2));
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int inputAsInt = Integer.valueOf(scanner.nextLine());
+        // double inputAsDouble = Double.valueOf(scanner.nextLine());
+        // boolean inutAsBool = Boolean.valueOf(scanner.nextLine());
+        //System.out.println(inputAsInt * 10);
+
+        
+        // System.out.println("Ievadi savu vārdu: ");
+        // String name = scanner.nextLine();
+
+        // System.out.println("Ievadi savu uzvārdu: ");
+        // String lastname = scanner.nextLine();
+
+        // System.out.println("Ievadi savu grupu: ");
+        // String group = scanner.nextLine();
+
+        // System.out.println("Students: "+ name + " " + lastname + ", grupa: " + group);
+
+
         //String userInput = scanner.nextLine();
         //System.out.println(userInput);
         //System.out.println("Ka tevi sauc?");
@@ -15,18 +53,6 @@ public class App
         // int someDigit = 99;
         // String end = ", Glebs Litvjakovs";
         //System.out.println("Mani EURO: " + (99 + 100));
-
-        System.out.println("Ievadi savu vārdu: ");
-
-        String name = scanner.nextLine();
-
-        System.out.println("Ievadi savu uzvārdu: ");
-        String lastname = scanner.nextLine();
-
-        System.out.println("Ievadi savu grupu: ");
-        String group = scanner.nextLine();
-
-        System.out.println("Students: "+ name + " " + lastname + ", grupa: " + group);
         // Diezgan labi )
     }
 
