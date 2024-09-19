788a838b94bbbd4ec52ba9a55adbc6254a6bf39f
23DP2MMitr <166109771+23DP2MMitr@users.noreply.github.com>
2024-09-19 10:24:59 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,34 +1,25 @@
 package lv.rvt;
 
 import java.util.*;
-
+// int gift = Integer.valueOf(scanner.nextLine());
 public class App 
 {
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Value of the gift?");
-
-        int gift = Integer.valueOf(scanner.nextLine());
-
-        if (gift < 5000){
-            System.out.println("No tax!");
-        }
-        else if (gift >= 5000 && gift <= 24999){
-            System.out.println("Tax: " + (100 + (gift - 5000) * 0.08));
-        }
-        else if (gift >= 25000 && gift <= 54999){
-            System.out.println("Tax: " + (1700 + (gift - 25000) * 0.1));
-        }
-        else if (gift >= 55000 && gift <= 199999){
-            System.out.println("Tax: " + (4700 + (gift - 55000) * 0.12));
-        }
-        else if (gift >= 200000 && gift <= 999999){
-            System.out.println("Tax: " + (22100 + (gift - 200000) * 0.15));
-        }
-        else if (gift >= 1000000){
-            System.out.println("Tax: " + (142100 + (gift - 1000000) * 0.17));
+        while (true) {
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+            }
+            if (number == 0) {
+                break;
+            }
+            if (number > 0) {
+                System.out.println(number * number);
+            }
         }
     }
+     
 }
