762077c0227640b87b34a576eb18a90812545932
23DP2MMitr <166109771+23DP2MMitr@users.noreply.github.com>
2024-09-26 10:24:56 +0000

    Repeating, breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,17 +9,31 @@ public class App
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a number: ");
         int nr = Integer.valueOf(scanner.nextLine());
-        int end = nr;
-        int start = 1;
-        if (nr == 0){
-            nr = 1;
-        }
-        else{
-            for(int i = start; i < end; i++) {
-                nr *= i;
+        int sum = nr + 1;
+        int kopa = 0;
+        int even = 0;
+        int odd = 0;
+        
+        while (true) {
+            if(nr == -1){
+                break;
+            }
+            nr = Integer.valueOf(scanner.nextLine());
+            sum += nr;
+            kopa++;
+            if (nr % 2 == 0) {
+                even ++;
+            }
+            else {
+                odd++;
             }
         }
-        System.out.println("Factorial: " + nr);
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: "+ kopa);
+        System.out.println("Average: "+ (double) sum / kopa);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
     }
      
 }
