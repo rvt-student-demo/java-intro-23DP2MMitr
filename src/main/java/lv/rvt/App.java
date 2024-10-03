963dba95aac44979558915ac533761d0cc601bfc
23DP2MMitr <166109771+23DP2MMitr@users.noreply.github.com>
2024-10-03 08:03:25 +0000

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,39 +1,22 @@
 package lv.rvt;
 
 import java.util.*;
-// int gift = Integer.valueOf(scanner.nextLine());
-public class App 
+
+public class App
 {
-    public static void main( String[] args )
+public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give a number: ");
-        int nr = Integer.valueOf(scanner.nextLine());
-        int sum = nr + 1;
-        int kopa = 0;
-        int even = 0;
-        int odd = 0;
-        
-        while (true) {
-            if(nr == -1){
-                break;
-            }
-            nr = Integer.valueOf(scanner.nextLine());
-            sum += nr;
-            kopa++;
-            if (nr % 2 == 0) {
-                even ++;
-            }
-            else {
-                odd++;
-            }
+        printUntilNumber(5);
+
+
+}
+    public static void printUntilNumber(int number) {
+        int i = 1;
+        while (i <= number) {
+            System.out.println(i);
+            i++;
         }
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: "+ kopa);
-        System.out.println("Average: "+ (double) sum / kopa);
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
-    }
-     
+}
+
 }
