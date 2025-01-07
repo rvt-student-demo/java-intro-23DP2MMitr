89c272c6538605c1925bed2388e221f02d5e6b87
Maksims Mitrofanovs <166109771+23DP2MMitr@users.noreply.github.com>
2025-01-07 13:11:31 +0000

    PersonManager FIX (Part 2)

--- src/main/java/lv/rvt/PaymentTerminal.java
+++ src/main/java/lv/rvt/PaymentTerminal.java
@@ -6,20 +6,34 @@ public class PaymentTerminal {
     private int heartyMeals;  // number of sold hearty meals
 
     public PaymentTerminal() {
-        money = 1000;
+        this.money = 1000;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
     }
 
     public double eatAffordably(double payment) {
-        if (payment - 2.5 >= 0) {
-            
+        double affordableMealPrice = 2.5;
+        if (payment >= affordableMealPrice) {
+            this.money += affordableMealPrice;
+            this.affordableMeals++;
+            return payment - affordableMealPrice;
+        } else {
+            return payment; // not enough money, return the full payment
         }
     }
 
     public double eatHeartily(double payment) {
-           
+        double heartyMealPrice = 4.3;
+        if (payment >= heartyMealPrice) {
+            this.money += heartyMealPrice;
+            this.heartyMeals++;
+            return payment - heartyMealPrice;
+        } else {
+            return payment; // not enough money, return the full payment
+        }
     }
 
     public String toString() {
-        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+        return "money: " + money + ", number of sold affordable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
     }
 }
